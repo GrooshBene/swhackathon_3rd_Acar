7522c0ed1595142ee623f352647e2e71d27facb2
박지수 <wltn000129@gmail.com>
2016-09-02 22:08:07 +0900

    route files added, index.js module setting done

--- index.js
+++ index.js
@@ -1,3 +1,55 @@
 /**
  * Created by GrooshBene on 2016. 9. 2..
  */
+
+var express = require('express');
+
+var mongoose = require('mongoose');
+
+var serveStatic = require('serve-static');
+
+var app = express();
+
+var bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({
+    extended : true
+}));
+
+var server = require('http').Server(app);
+
+
+var schema = mongoose.Schema;
+
+mongoose.connect("mongodb://localhost:27017/acar", function (err) {
+    if(err){
+        console.log("MongoDB Error!");
+        throw err;
+    }
+});
+
+var userSchema = new schema({
+    _id : {
+        type : String
+    },
+    name : {
+        type : String
+    },
+
+    profile : {
+        type : String
+    },
+
+    gcm_token : {
+        type : String
+    }
+});
+
+var User = mongoose.model('user', userSchema);
+
+server.listen(7800);
+console.log("Server Running At Port 7800");
+
+require('./route/oauth')(app, User);
+
+require('./route/user')(app, User);
---
+++ route/user.js
@@ -0,0 +1,3 @@
+/**
+ * Created by GrooshBene on 2016. 9. 2..
+ */
